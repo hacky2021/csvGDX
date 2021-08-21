ccafc4f18492ade0f07d4c2cc189d792e9f0690f
hacky2021 <87270550+hacky2021@users.noreply.github.com>
2021-08-21 11:07:47 +0900

    Update csvGDX.py

--- csvGDX.py
+++ csvGDX.py
@@ -20,6 +20,28 @@ class copen:
         for s in self.fileList:
             self.i+=1
             self.fileList[self.i]=self.fileList[self.i].split(',')
+    def setCell(self,c1,c2):
+        if self.mode==1:
+            self.itemsT=re.match(r"([A-Z]+)([0-9]+)",c1.upper(), re.I).groups()
+            self.items1=[0,0]
+            self.items1[0]=int(self.itemsT[0].replace("A","1").replace( "B","2").replace( "C","3").replace( "D","4").replace( "E","5").replace( "F","6").replace( "G","7").replace( "H","8").replace( "I","9").replace( "J","a").replace( "K","b").replace( "L","c").replace( "M","d").replace( "N","e").replace( "O","f").replace( "P","g").replace( "Q","h").replace( "R","i").replace( "S","j").replace( "T","k").replace( "U","l").replace( "V","m").replace( "W","n").replace( "X","o").replace( "Y","p").replace( "Z","q"),27)-1
+            self.items1[1]=int(self.itemsT[1])-1
+        elif self.mode==2:
+            self.items1=[0,0]
+            self.items1[0]=int(c1[:c1.find(":")])
+            self.items1[1]=int(c1[c1.find(":")+1:len(c1)])
+        self.fileList[self.items1[1]][self.items1[0]]=str(c2)
+    def getCell(self,c1):
+        if self.mode==1:
+            self.itemsT=re.match(r"([A-Z]+)([0-9]+)",c1.upper(), re.I).groups()
+            self.items1=[0,0]
+            self.items1[0]=int(self.itemsT[0].replace("A","1").replace( "B","2").replace( "C","3").replace( "D","4").replace( "E","5").replace( "F","6").replace( "G","7").replace( "H","8").replace( "I","9").replace( "J","a").replace( "K","b").replace( "L","c").replace( "M","d").replace( "N","e").replace( "O","f").replace( "P","g").replace( "Q","h").replace( "R","i").replace( "S","j").replace( "T","k").replace( "U","l").replace( "V","m").replace( "W","n").replace( "X","o").replace( "Y","p").replace( "Z","q"),27)-1
+            self.items1[1]=int(self.itemsT[1])-1
+        elif self.mode==2:
+            self.items1=[0,0]
+            self.items1[0]=int(c1[:c1.find(":")])
+            self.items1[1]=int(c1[c1.find(":")+1:len(c1)])
+        return self.fileList[self.items1[1]][self.items1[0]]
     def addCell(self,c1,c2,c3):
         if self.mode==1:
             self.itemsT=re.match(r"([A-Z]+)([0-9]+)",c1.upper(), re.I).groups()
